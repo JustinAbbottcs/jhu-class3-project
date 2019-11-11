aa5a2a6b2da67b016cc5e345a920c4e13d5a1163
Justin Abbott <jabbo4@brockport.edu>
2019-11-11 17:41:43 -0500

    Adding environment specification and var name sub

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+##You should be in the directory where you stored the dataset
 x_test <- read.delim("./UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "", dec = ".")
 y_test <- read.delim("./UCI HAR Dataset/test/Y_test.txt", col.names = c("Activity"),  header = FALSE, sep = "")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = c("Subject"), header = FALSE, sep = "")
@@ -30,9 +31,15 @@ filtered_data <- data[grepl("mean|std", names(data), ignore.case = T)]
 #variables of interest
 filtered_data <- cbind(filtered_data, Activity=data$Activity, Subject=data$Subject)
 
-#Replace Activity variable observations with more descriptive values
+#Replace variables and Acvtivity observations with more descriptive values
 clean_data <- filtered_data %>% 
   mutate(Activity=sapply(filtered_data$Activity, function(x){ x <- activity_labels[x, 2] }))
+
+names(clean_data) <- gsub("^t", "time", names(clean_data))
+names(clean_data) <- gsub("Acc", "Acceleration", names(clean_data))
+names(clean_data) <- gsub("^f", "freq", names(clean_data))
+names(clean_data) <- gsub("BodyBody", "Body", names(clean_data))
+
 
 #Tidying up data
 melted_dataset <- clean_data %>%
